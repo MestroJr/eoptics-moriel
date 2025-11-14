4d78afce6e88b6097fba3a2c8cc12c140b62258f
MestroJr <martosjr154@gmail.com>
2025-11-13 23:44:36 -0600

    Create check.js

---
+++ pages/api/check.js
@@ -0,0 +1,9 @@
+import { supabase } from "/lib/supabaseClient";
+
+export default async function handler(req, res) {
+  const { data, error } = await supabase.from("usuarios").select("*").limit(1);
+
+  if (error) return res.status(500).json({ error });
+
+  res.status(200).json({ ok: true, data });
+}
